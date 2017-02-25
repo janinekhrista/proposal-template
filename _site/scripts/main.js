8a411f58b475888800ad7365e98231a19cb6c361
janinekhrista <kristandres@yahoo.com>
2017-02-24 18:00:47 -0800

    2017-02-24 B
    Hover fixed to cover whole photo, but effect is interfering with
    navigation. Timeline content imported, but javascript not yet included.

--- _site/scripts/main.js
+++ _site/scripts/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function()) {
 
-  // jQuery Code Here
+  var flik = $('.myTimeline').flikTimeline({
+ style: 'style1',
+ showDate: true,
+ showTitle: true,
+
+ requestData: {
+     type: 'GET',
+     url: 'http://cidcode.net/timelineapi/timeline_1',
+     data: '',
+     onRequestDone: function (data) {
+         console.log('onRequestDone');
+      },
+      onRequestFail: function (e) {
+         console.log('onRequestFail');
+     }
+  },
+  loadingMessage: "<center style='color:white'>loading</center>",
+  delay: 500,effect:'basic'
+  });
 
 });
